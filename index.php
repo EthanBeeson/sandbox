3a9576cd82f7ddcfbbc04fc150eb118fc12da9e7
Ethan Beeson <ethanmbeeson@gmail.com>
2022-05-05 15:36:52 -0600

    [wip] updates to website v.2

--- index.php
+++ index.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HTML 5 Boilerplate</title>
+    <title>First Website</title>
     <link rel="stylesheet" href="main.css">
   </head>
   <body>
 	<script src="main.js"></script>
+	<h1>Ethan's WebPage</h1>
+<p> 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas accumsan. Eget nunc scelerisque viverra mauris in aliquam. Tristique senectus et netus et malesuada fames ac turpis egestas. Malesuada fames ac turpis egestas. Nunc mi ipsum faucibus vitae aliquet nec. Orci dapibus ultrices in iaculis nunc sed augue lacus. Tellus id interdum velit laoreet id donec ultrices. Auctor neque vitae tempus quam pellentesque. Eget gravida cum sociis natoque penatibus et magnis. Fames ac turpis egestas integer eget aliquet. Eget mauris pharetra et ultrices neque ornare aenean. Imperdiet dui accumsan sit amet nulla facili blah blah"</p>	
   </body>
 </html>
