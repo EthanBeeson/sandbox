953e4f16fc0c00324254cdc4021c68d66c464ef0
Ethan Beeson <ethanmbeeson@gmail.com>
2022-05-10 09:56:36 -0600

    [WIP] webiste version 3

--- index.php
+++ index.php
@@ -13,7 +13,15 @@
 		<h1>Ethan's WebPage</h1>
 	</header>
 	<img src="https://3.bp.blogspot.com/-Vxpx9a4a75U/UuJd81EYS-I/AAAAAAAACXk/NZWg4oJYpgc/s1600/Dog-Picture6.jpg" alt="dog">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas accumsan. Eget nunc scelerisque viverra mauris in aliquam. Tristique senectus et netus et malesuada fames ac turpis egestas. Malesuada fames ac turpis egestas. Nunc mi ipsum faucibus vitae aliquet nec. Orci dapibus ultrices in iaculis nunc sed augue lacus. Tellus id interdum velit laoreet id donec ultrices. Auctor neque vitae tempus quam pellentesque. Eget gravida cum sociis natoque penatibus et magnis. Fames ac turpis egestas integer eget aliquet. Eget mauris pharetra et ultrices neque ornare aenean. Imperdiet dui accumsan sit amet nulla facili blah blah"</p>	
+
+<div>
+<div id="button">useless button</div>
+</div>
+		<p class = "first">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas accumsan. Eget nunc scelerisque viverra mauris in aliquam. Tristique senectus et netus et malesuada fames ac turpis egestas. Malesuada fames ac turpis egestas. Nunc mi ipsum faucibus vitae aliquet nec. Orci dapibus ultrices in iaculis nunc sed augue lacus. Tellus id interdum velit laoreet id donec ultrices. Auctor neque vitae tempus quam pellentesque. Eget gravida cum sociis natoque penatibus et magnis. Fames ac turpis egestas integer eget aliquet. Eget mauris pharetra et ultrices neque ornare aenean. Imperdiet dui accumsan sit amet nulla facili blah blah"</p>	
+ <p class = "second">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas accumsan. Eget nunc scelerisque viverra mauris in aliquam. Tristique senectus et netus et malesuada fames ac turpis egestas. Malesuada fames ac turpis egestas. Nunc mi ipsum faucibus vitae aliquet nec. Orci dapibus ultrices in iaculis nunc sed augue lacus. Tellus id interdum velit laoreet id donec ultrices. Auctor neque vitae tempus quam pellentesque. Eget gravida cum sociis natoque penatibus et magnis. Fames ac turpis egestas integer eget aliquet. Eget mauris pharetra et ultrices neque ornare aenean. Imperdiet dui accumsan sit amet nulla facili blah blah"</p>
+
+ <p class = "third">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas accumsan. Eget nunc scelerisque viverra mauris in aliquam. Tristique senectus et netus et malesuada fames ac turpis egestas. Malesuada fames ac turpis egestas. Nunc mi ipsum faucibus vitae aliquet nec. Orci dapibus ultrices in iaculis nunc sed augue lacus. Tellus id interdum velit laoreet id donec ultrices. Auctor neque vitae tempus quam pellentesque. Eget gravida cum sociis natoque penatibus et magnis. Fames ac turpis egestas integer eget aliquet. Eget mauris pharetra et ultrices neque ornare aenean. Imperdiet dui accumsan sit amet nulla facili blah blah"</p>
+
   </body>
 
 </html>
